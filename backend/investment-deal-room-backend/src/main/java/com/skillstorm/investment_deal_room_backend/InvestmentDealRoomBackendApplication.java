30a6712a85056b07ed5d42bfbab1f1e499b592d3
Alejandro Sanroman <77080173+asanroNA@users.noreply.github.com>
2026-04-20 14:30:30 -0500

    backend basic init

---
+++ backend/investment-deal-room-backend/src/main/java/com/skillstorm/investment_deal_room_backend/InvestmentDealRoomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.investment_deal_room_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentDealRoomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentDealRoomBackendApplication.class, args);
+	}
+
+}
---
+++ backend/investment-deal-room-backend/src/test/java/com/skillstorm/investment_deal_room_backend/InvestmentDealRoomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.investment_deal_room_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentDealRoomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
