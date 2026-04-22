e66e6968d3ae9c0f20e2f15e10a3ebb4988bee7e
Aiden <aidenkwallen@gmail.com>
2026-04-22 10:05:14 -0500

    Set up deal frontend architecture (module, components, structure)

---
+++ frontend/investment-deal-room-frontend/src/app/deal/components/deal-form/deal-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deal-form',
+  imports: [],
+  templateUrl: './deal-form.html',
+  styleUrl: './deal-form.css',
+})
+export class DealForm {
+
+}
