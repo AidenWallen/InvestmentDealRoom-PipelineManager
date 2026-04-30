c1f7a7f169f2c4abc9f836454b76b1dde0d03e14
Alejandro Sanroman <77080173+asanroNA@users.noreply.github.com>
2026-04-30 09:11:51 -0500

    completed sidebar with implemented routes

---
+++ frontend/investment-deal-room-frontend/src/app/deal/pages/counterparty-page/counterparty-page.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({  
+    selector: 'app-counterparty-page',
+    standalone: true,
+    imports : [],
+    templateUrl: './counterparty-page.html',
+})
+export class CounterpartyPage {
+  constructor() {}
+}
